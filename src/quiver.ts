6f6b505ef1267556ceadac88fe3e1ba352edfc76
achilles schmelzer <a@achilles.engineering>
2024-09-06 12:00:58 -0700

    types, types, types

--- src/quiver.ts
+++ src/quiver.ts
@@ -1,28 +1,35 @@
-import { create } from "domain";
+export type QuiverMiddleware<CtxIn, CtxOut> = CtxIn extends undefined
+  ? () => CtxOut
+  : (ctx: CtxIn) => CtxOut;
 
-export type QuiverFunction<I, O> = (i: I) => O;
+export type QuiverFunction<I, CtxIn, O> = CtxIn extends undefined
+  ? (i: I) => O
+  : (i: I, ctx: CtxIn) => O;
 
-export type QuiverRouter<I> = {
-  middleware?: QuiverFunction<I, any>;
+export type QuiverRouter<CtxIn> = {
+  middleware?: QuiverMiddleware<CtxIn, any>;
   routes: {
     [key: string]: QuiverRouter<any> | QuiverRoute<any>;
   };
 };
 
-export type QuiverRoute<I> = {
-  middleware?: QuiverFunction<I, any>;
-  route: QuiverFunction<any, any>;
+export type QuiverRoute<CtxIn> = {
+  middleware?: QuiverMiddleware<CtxIn, any>;
+  route: QuiverFunction<any, CtxIn, any>;
 };
 
-export const createFunction = <F extends QuiverFunction<any, any>>(f: F): F => {
+export const createMiddleware = <F extends QuiverMiddleware<any, any>>(
+  f: F,
+): F => {
   return f;
 };
 
 const createRoute = <
-  R extends QuiverFunction<ReturnType<M>, any>,
-  M extends QuiverFunction<any, any> = QuiverFunction<unknown, unknown>,
+  CtxOut,
+  F extends QuiverFunction<any, CtxOut, any> = QuiverFunction<any, CtxOut, any>,
+  M extends QuiverMiddleware<any, CtxOut> = QuiverMiddleware<unknown, CtxOut>,
 >(
-  route: R,
+  route: F,
   middleware: M,
 ) => {
   return {
@@ -35,7 +42,7 @@ const createRouter = <
   R extends {
     [key: string]: QuiverRouter<ReturnType<M>> | QuiverRoute<ReturnType<M>>;
   },
-  M extends QuiverFunction<any, any> = QuiverFunction<unknown, unknown>,
+  M extends QuiverMiddleware<any, any> = QuiverMiddleware<unknown, unknown>,
 >(
   routes: R,
   middleware: M,
@@ -51,15 +58,38 @@ const createRouter = <
 const math = createRouter(
   {
     add: createRoute(
-      (args: { a: number; b: number }) => {
-        return args.a + args.b;
-      },
-      (m: string) => ({ a: m.length, b: 10 }),
+      (i: string) => i,
+      (args: { a: number; b: number }) => args,
+    ),
+    sub: createRoute<{ a: number; b: number }>(
+      (i, args: { a: number; b: number }) => args.a - args.b,
+      (args: { a: number; b: number }) => args,
     ),
   },
-  (s: string) => s,
+  (args: { a: number; b: number }) => args,
+);
+const hello = createRoute<{ name: string }>(
+  (i: any, args: { name: string }) => `Hello, ${args.name}, ${i}!`,
+  (args) => args,
 );
 
-const d = createRouter({ math }, () => 10);
+const m = hello.middleware({ name: "world" });
+const v = hello.route(null, m);
 
-d.routes.math;
+const pub = createRouter(
+  {
+    hello: createRoute(
+      (i, args: { name: number }) => `Hello, ${args.name}!`,
+      (args: { name: number } | { x: null }) => args,
+    ),
+  },
+  (args: { name: number }) => args,
+);
+
+const app = createRouter(
+  {
+    math,
+    pub,
+  },
+  (args: { name: string }) => args,
+);
