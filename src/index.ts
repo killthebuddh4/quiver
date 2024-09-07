c80ee58e0c1d61077783d4431c28544d289179a1
achilles schmelzer <a@achilles.engineering>
2024-09-07 11:16:30 -0700

    add q.client

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { createApp } from "./quiver/createApp.js";
 import { createFunction } from "./quiver/createFunction.js";
 import { createServer } from "./quiver/createServer.js";
+import { createClient } from "./quiver/createClient.js";
 
 export const q = {
   app: createApp,
   function: createFunction,
   server: createServer,
+  client: createClient,
 };
 
 export default q;
